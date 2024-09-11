e93f5a83b65d1ffea1dda5d8bea8fbaad96fd42b
shakmatton <118413324+shakmatton@users.noreply.github.com>
2024-09-11 14:25:10 -0300

    Create poligonos.c

---
+++ poligonos.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    int lados;
+       
+    scanf("%d", &lados);
+    printf("%d\n", lados - 2);    // Triangles quantity = Poligon side number
+
+    return 0;
+}
