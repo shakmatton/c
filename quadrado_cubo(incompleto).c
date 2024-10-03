07856f2917d250ac0d8ab327174338cfa51dfd8a
shakmatton <118413324+shakmatton@users.noreply.github.com>
2024-10-03 11:47:10 -0300

    Create quadrado_cubo(incompleto).c

---
+++ quadrado_cubo(incompleto).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.c>
+
+int main() {
+
+    int num;
+    
+    scanf("%d", &x);
+    for (int i = 1; i <= num; i++) {
+        for (int j = 1; i <= 3; j++) {     // 3 as a fixed value of rows
+            printf("%d ", j);
+            j = pow(j, 3)
+        }
+        printf("\n");
+    }
+   
+    
+    return 0;
+
+}  
