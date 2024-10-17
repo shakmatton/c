1b4a304880e75eb1e21b9e0641fe3354be3b9704
shakmatton <118413324+shakmatton@users.noreply.github.com>
2024-10-17 19:15:25 -0300

    Create somaImparesConsecutivos.c

---
+++ somaImparesConsecutivos.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Read 2 integers X and Y. 
+// Then, calculate and show the sum of the odd numbers between them.
+
+
+int somaOdds (int x, int y) {      // sums up all odd number in the XY interval 
+
+    int total = 0;
+    
+    for (int i = x + 1 ; i < y; i++) {    // (12 + 1) -> 18
+        if (i % 2 != 0)  total += i;      // sums up only odd numbers
+    }
+
+    return total;
+}
+
+
+int main() {
+
+    int x, y, aux, sum;
+
+    scanf("%d %d", &x, &y);
+
+    // neighbour integers have no intermediary number between them.
+    if (abs(x - y) < 2) printf("0\n");  
+
+    else {
+        if (x > y) {     // avoid looping from 15 -> 2  
+            aux = x;
+            x = y;
+            y = aux;     // better looping from 2 -> 15
+        }
+
+        sum = somaOdds(x, y);
+        printf("%d\n", sum);   // prints sum of the odd numbers
+    }
+
+    return 0;
+}
