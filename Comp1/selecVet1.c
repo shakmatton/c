1d96a0ab4e972d0a5b9a42aabd480ab19bc774d1
shakmatton <ezio.auditore0@protonmail.com>
2024-11-17 23:35:19 -0300

    added new file

---
+++ Comp1/selecVet1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define MAX 100
+
+
+void readArray(float *p) {                    // reads array using pointers
+    
+    for (int i = 0; i < MAX; i++) {
+        scanf("%f", p);                     // p: works as &(variable) address, in scanf()
+        p++;                                // pointer advances to next index
+    }
+}
+
+
+void writeArray(float *p) {
+    
+    for (int i = 0; i < MAX; i++) {
+        
+        if (*p <= 10.0) {                         // we want all values pointed by pointer under or equal 10
+            printf("A[%d] = %.1f\n", i, *p);       // prints pointer value
+        }
+
+        p = p + 1;                              // same as p++
+    }
+}
+
+
+int main() {
+
+    float A[MAX];
+
+    readArray(A);
+    writeArray(A);
+
+    return 0;
+}
